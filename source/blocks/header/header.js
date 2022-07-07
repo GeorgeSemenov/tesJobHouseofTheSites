1ce5d4ad1f52103aeb500b9aea8d2e70460f292e
GeorgeSemenov <zhora.sem@yandex.ru>
2022-07-07 11:33:56 +0300

    almost ended up with default header

--- source/blocks/header/header.js
+++ source/blocks/header/header.js
@@ -1,32 +1,23 @@
+//упрощённая схема аккордеона находится тут https://codepen.io/Goshanbo/pen/ExEPOXg
 $(document).ready(function(){
-  //функция для подсчёта высоты хедера
-  //В мобильной версии высота хедера (headerTotalHeight) является суммой из 3х составляющих 
-  //видимая часть - headerVisiblePartHeight - верхушка в которой находится сэндвич, логотип и кнопка поиска(пупа)
-  //высота раздела со страничками - pagesHeight
-  //высота раздела с сервисами - servicesHeight
-  $(`.header__service-href_dropdown`).parent().height($(`.header__service-href_dropdown`).height());//Выставляем всем элементам списка в nav у которых есть выпадающий список высоту равную заголовку выпадающего списка
-  let headerVisiblePartHeight = 34;
-  let pagesHeight , servicesHeight , headerTotalHeight;
-  calculateHeight();
+  let pages = $(`.header__nav-pages`);
+  let services = $(`.header__nav-services`);
   window.addEventListener('resize', function(event) {//При изменении ширины экрана происходит перерасчёт высоты хедера
-    calculateHeight();
-    if ($(window).width() >= 767)
-      $('.header').height('auto');
-    else
-      if ($('.header').hasClass('header_expanded')){
-        $('.header').height(headerTotalHeight);
-      }else{
-        $('.header').height(headerVisiblePartHeight);
+      if (!$('.header').hasClass('header_expanded') && $(window).width() <= 767){//для мобилок и при свёрнутом хедере
+        pages.css({"display": "none"});
+        services.css({"display": "none"});
+      }
+      if($(window).width() > 767){
+        pages.css({"display": "block"});
+        services.css({"display": "block"});
       }
   }, true);
 
   $('.header__sandwich-ico-container').click(function(){//При нажатии на бутерброд будет пересчитанна высота хедера
+    pages.slideToggle();
+    services.slideToggle();
+
     $('.header').toggleClass('header_expanded');
-    if ($('.header').hasClass('header_expanded')){
-      $('.header').height(headerTotalHeight);
-    }else{
-      $('.header').height(headerVisiblePartHeight);
-    }
   });
 
   //функция для открытия и закрытия выпадающего списка и подсчёта высоты хедера на мобильных версиях
@@ -34,21 +25,13 @@ $(document).ready(function(){
     let submenu = $(this).siblings(".header__service-submenu");
     $(this).toggleClass('header__service-href_dropdown_pic-rotated');
     submenu.toggleClass('header__service-submenu_show');
+    submenu.slideToggle();
     if ($(window).width() <= 767 && submenu.hasClass(`header__service-submenu_show`)){
       //подсчитываем высоту элемента списка в котором содержится под меню - это высота заголовка подменю и высота самого подменю
-      submenu.height(submenu.height() + submenu.siblings().height())
+      // submenu.parent().height(submenu.height() + submenu.siblings().height())
     }else if($(window).width() <= 767 && !submenu.hasClass(`header__service-submenu_show`)){
       //убираем высоту подменю
-      submenu.height(submenu.siblings().height())
+      // submenu.parent().height(submenu.siblings().height())
     }
-    calculateHeight();
-    $('.header').height(headerTotalHeight);
   })
-
-  function calculateHeight(){//подсчитывает headerTotalHeight
-    pagesHeight = $(".header__nav-pages").height();
-    servicesHeight = $(".header__nav-services").height();
-    headerTotalHeight = pagesHeight + servicesHeight + headerVisiblePartHeight;
-  }
-
 })
