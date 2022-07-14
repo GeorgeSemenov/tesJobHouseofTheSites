ee1b4c7068227eaf87b78d15ac0e187bd53f9771
GeorgeSemenov <zhora.sem@yandex.ru>
2022-07-14 11:30:33 +0300

    main-screen is coming

--- source/blocks/header/header.js
+++ source/blocks/header/header.js
@@ -1,22 +1,27 @@
 //упрощённая схема аккордеона находится тут https://codepen.io/Goshanbo/pen/ExEPOXg
 $(document).ready(function(){
-  let pages = $(`.header__nav-pages`);
+  // let pages = $(`.header__nav-pages`); 
+  // В этой версии шапки - страничек нет;
   let services = $(`.header__nav-services`);
+  let contacts = $(`.header__contacts`);
   let submenu = $(`.header__service-submenu`);
+  if($(window).width() < 767 ){
+    contacts.css({"display" : "none"})//При запуске на мобилках контакты должны быть скрытыми.
+  }
   window.addEventListener('resize', function(event) {
     if($(window).width() > 767 ){
-      pages.css({"display" : "block"});
+      contacts.css({"display" : "flex"});
       services.css({"display" : "block"});
     }else{//для мобилок
       if(!$(`.header`).hasClass(`header_expanded`)){//Если хедер свёрнут
-        pages.css({"display" : "none"});
+        contacts.css({"display" : "none"});
         services.css({"display" : "none"});
       }
     }
   }, true);
 
   $('.header__sandwich-ico-container').click(function(){//При нажатии на бутерброд будет пересчитанна высота хедера
-    pages.slideToggle();
+    contacts.slideToggle();
     services.slideToggle();
 
     $('.header').toggleClass('header_expanded');
