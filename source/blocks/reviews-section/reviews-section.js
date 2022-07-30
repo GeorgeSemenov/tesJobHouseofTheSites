cde7ada0c6ff01283b44932f27bb2a18360b7071
GeorgeSemenov <zhora.sem@yandex.ru>
2022-07-30 06:17:43 +0300

    swiper improved

---
+++ source/blocks/reviews-section/reviews-section.js
@@ -0,0 +1,13 @@
+// import Swiper, {Pagination} from 'swiper';
+$(document).ready(function(){
+  // console.log("hello");
+  // swiper = new Swiper('.reviews-section__reviews', {
+  //   modules: [Pagination],
+  //   slidesPerView: 1,
+  //   spaceBetween: 0,
+  //   loop: true,
+  //   pagination: {
+  //     el: '.reviews-section__review-pagination',
+  //   }
+  // });
+})
