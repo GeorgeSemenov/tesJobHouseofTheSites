e2823378a4b4b9614d90ad3dd659f2705df5da6e
GeorgeSemenov <zhora.sem@yandex.ru>
2022-07-30 14:13:17 +0300

    accordeons improoved

--- source/blocks/reviews-section/reviews-section.js
+++ source/blocks/reviews-section/reviews-section.js
@@ -1,13 +1,16 @@
-// import Swiper, {Pagination} from 'swiper';
+import Swiper, {Pagination, Autoplay} from 'swiper';
 $(document).ready(function(){
-  // console.log("hello");
-  // swiper = new Swiper('.reviews-section__reviews', {
-  //   modules: [Pagination],
-  //   slidesPerView: 1,
-  //   spaceBetween: 0,
-  //   loop: true,
-  //   pagination: {
-  //     el: '.reviews-section__review-pagination',
-  //   }
-  // });
+  let swiper = new Swiper('.reviews-section__reviews', {
+    modules: [Pagination, Autoplay],
+    slidesPerView: 1,
+    spaceBetween: 0,
+    // autoplay: {
+    //   delay: 2500,
+    // },
+    loop: true,
+    pagination: {
+      el: '.reviews-section__review-pagination',
+      clickable: true,
+    }
+  });
 })
--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -5,4 +5,5 @@ import '../../blocks/header/header.js';
 import '../../blocks/main-screen/main-screen.js';
 import '../../blocks/ssl-cert/ssl-cert.js';
 import '../../blocks/reviews-section/reviews-section.js';
+import '../../blocks/faq-section/faq-section.js';
 
