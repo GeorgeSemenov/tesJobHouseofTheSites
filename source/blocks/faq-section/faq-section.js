db733118d853433e394dabebdcb32c9303b91368
GeorgeSemenov <zhora.sem@yandex.ru>
2022-08-01 11:44:44 +0300

    last changes

--- source/blocks/faq-section/faq-section.js
+++ source/blocks/faq-section/faq-section.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-  
+  $('.faq-section__accordeon-title-container').click(function(){
+    $(this).parent().toggleClass("faq-section__accordeon_opened");
+    $(this).next('.faq-section__accordeon-text').slideToggle(300);
+  })
 })
